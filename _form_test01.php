c884028c53f81fbd454c7f3617f4372d5d0aa032
luke <weu.che0812@gmail.com>
2019-08-26 13:47:34 +0800

    checkboxes from array

--- _form_test01.php
+++ _form_test01.php
@@ -1,3 +1,14 @@
+<?php 
+$data1 = [
+    '2' => '蘋果',
+    '3' => '鳳梨',
+    '5' => '香蕉',
+    '9' => '楊桃',
+];
+
+
+?>
+
 <?php require __DIR__ . '/__html_header.php' ?>
 
 <div class="container">
@@ -19,17 +30,14 @@
             <label class="form-check-label" for="exampleCheck1">Check me out</label>
         </div>
 
+        <?php foreach($data1 as $k=>$v): ?>
         <div class="form-group form-check">
-            <input type="checkbox" class="form-check-input" value="蘋果" id="fruit1" name="fruits[]">
-            <label class="form-check-label" for="fruit1">蘋果</label>
-<br>
-            <input type="checkbox" class="form-check-input" value="鳳梨" id="fruit2" name="fruits[]">
-            <label class="form-check-label" for="fruit2">鳳梨</label>
-            <br>
-            <input type="checkbox" class="form-check-input" value="香蕉" id="fruit3" name="fruits[]">
-            <label class="form-check-label" for="fruit3">香蕉</label>
-        </div>
+            <input type="checkbox" class="form-check-input" name="fruits[]"
+                        value="<?= $k ?> " id="fruit-a-<?= $k ?>" >
+            <label class="form-check-label" for="fruit-a-<?= $k ?>"><?= $v ?></label>
 
+        </div>
+        <?php endforeach ?>
 
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>
